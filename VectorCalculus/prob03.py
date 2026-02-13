7618643d7c2f0baf76812c63750136194e55a36e
aalopera_ncstate <aalopera@ncsu.edu>
2026-02-13 09:34:52 -0500

    Finalize VectorCalculus Problem 03

--- VectorCalculus/prob03.py
+++ VectorCalculus/prob03.py
@@ -4,7 +4,23 @@ def dinvA(x):
     
     ### <--- START OF YOUR CODE
 
-    dinvA = np.array(0)
+    # Define A(x)
+    A = np.array([
+        [1, x**3 + 3],
+        [x**4 + x**2 + 3, x**2 + 1]
+    ], dtype=float)
+
+    # Derivative A'(x)
+    A_prime = np.array([
+        [0, 3*x**2],
+        [4*x**3 + 2*x, 2*x]
+    ], dtype=float)
+
+    # Compute inverse of A
+    A_inv = np.linalg.inv(A)
+
+    # Apply formula: -A^{-1} A' A^{-1}
+    dinvA = -A_inv @ A_prime @ A_inv
 
     ### END OF YOUR CODE --->
 
